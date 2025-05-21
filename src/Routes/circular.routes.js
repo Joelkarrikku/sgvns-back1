96508b51a3426774a5c2e3b3ef4e3ae55e94d7a4
Joel Thomas <joelsway19@gmail.com>
2025-05-21 17:02:56 +0530

    Updated backend code

--- src/Routes/circular.routes.js
+++ src/Routes/circular.routes.js
@@ -9,7 +9,7 @@ const authenticateAdmin = require('../Middlewares/admin.middleware');
 router.post(
   '/',
   verifyToken,
-  authorizeRole('Admin'),
+  authenticateAdmin('Admin'),
   upload.single('file'),
   async (req, res) => {
     try {
