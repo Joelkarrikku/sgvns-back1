b52e1a9de6f88a89148e96700112de956b6f2320
Joel Thomas <joelsway19@gmail.com>
2025-05-22 03:55:20 +0530

    Updated backend code

--- src/Middlewares/upload.middleware.js
+++ src/Middlewares/upload.middleware.js
@@ -1,19 +1,20 @@
-// src/Middlewares/upload.middleware.js
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const multer = require('multer');
-const cloudinary = require('../config/cloudinary'); // ✅ Correct path
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const multer = require("multer");
+const cloudinary = require("../config/cloudinary");
 
 const storage = new CloudinaryStorage({
-  cloudinary, // ✅ already v2 instance
-  params: {
-    folder: 'sgvns_uploads', // customize folder as needed
-    allowed_formats: ['jpg', 'png', 'jpeg', 'pdf'],
-    public_id: (req, file) => {
-      return `${Date.now()}-${file.originalname}`; // Optional: unique name
-    },
+  cloudinary: cloudinary,
+  params: async (req, file) => {
+    const isPdf = file.mimetype === "application/pdf";
+    return {
+      folder: "SGVNS-Docs",
+      resource_type: isPdf ? "raw" : "image",
+      format: isPdf ? "pdf" : undefined,
+      public_id: file.originalname.split(".")[0],
+      access_mode: "public", // <<< THIS is important
+    };
   },
 });
 
 const upload = multer({ storage });
-
 module.exports = upload;
