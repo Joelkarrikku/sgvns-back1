88f5389800c795515ff16474b392e0c52e8625e5
Joel Thomas <joelsway19@gmail.com>
2025-05-21 20:55:33 +0530

    Updated backend code

--- src/Routes/auth.routes.js
+++ src/Routes/auth.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../Controllers/auth.controller'); // ✅ Corrected path
-const { verifyToken } = require('../middlewares/auth.middleware');
+const { verifyToken } = require('../Middlewares/auth.middleware');
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.get('/profile', verifyToken, authController.getAdminProfile);
