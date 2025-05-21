ae8b5c332cdc1f2704fc7b3390f428a1b03c004a
Joel Thomas <joelsway19@gmail.com>
2025-05-21 13:10:40 +0530

    Updated backend code

--- src/Routes/notification.routes.js
+++ src/Routes/notification.routes.js
@@ -26,7 +26,7 @@ router.post("/", verifyToken, authorizeRole("Admin"), async (req, res) => {
         const notification = new Notification({
             message,
             recipient,
-            type: type || "info",
+            type: type || "",
         });
 
         await notification.save();
